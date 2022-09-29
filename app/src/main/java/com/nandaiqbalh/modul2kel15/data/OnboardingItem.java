0943d4f73820a38b86b06d6987ac5e52add7cf43
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-09-29 09:28:35 +0700

    Landing page & splashscreen

---
+++ app/src/main/java/com/nandaiqbalh/modul2kel15/data/OnboardingItem.java
@@ -0,0 +1,30 @@
+package com.nandaiqbalh.modul2kel15.data;
+
+public class OnboardingItem {
+    private int image;
+    private String description, title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
